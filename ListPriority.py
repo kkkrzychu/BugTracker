f58b7a8922ca6228358469b649237b595a4753de
Krzysztof Dziemiańczyk <dziemianczyk.k@gmail.com>
2026-08-07 13:30:39 +0200

    priorytety nauczone

--- ListPriority.py
+++ ListPriority.py
@@ -1,4 +1,14 @@
 priorities = ["Low", "Medium", "High"]
 
-wybor = input("Podaj numer: ")
+while True:
 
+    try:
+        wybor = int(input("Podaj numer: "))
+        if wybor not in [1,2,3]:
+                print("niepoprawny priorytet, jeszcze raz")
+        
+        else:
+            print(priorities[wybor-1])
+            break
+    except ValueError:
+        print("niepoprawny priorytet, jeszcze raz")
